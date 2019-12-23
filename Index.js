8706e777a6d46d7e5afdf98f7883d2b8af5cbfe1
Gh0stReaper1888 <42793451+Gh0stReaper1888@users.noreply.github.com>
2019-12-23 18:09:29 +0000

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 const bot = new discord.Client()
-const token = "NjU4NzI3NDIwNDcxMjc5NjQ3.XgD9vg.BAvcZhA2QrWO1H7R8fzkVLaKxaE"
+// const token = "NjU4NzI3NDIwNDcxMjc5NjQ3.XgD9vg.BAvcZhA2QrWO1H7R8fzkVLaKxaE"
 
 bot.on("message", message => {
 
@@ -41,4 +41,4 @@ bot.on("message", message => {
     }
 })
 
-bot.login(token)
+bot.login(process.env.BOT_TOKEN)
